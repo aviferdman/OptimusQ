7076f9dd1d36730584d0b8c229bd6243b19e1eca
Gil <gilhat@post.bgu.ac.il>
2021-12-19 20:35:28 +0200

    Images

--- ImageService/main.py
+++ ImageService/main.py
@@ -31,8 +31,8 @@ Retrieve list of images according to search's keywords
 Parameters
 ----------
 stock -> str: name of images stock to use (pixable \ shutterstock)
-keywords -> str: keywords to search for images
-max_images -> int: max number of images to return
+keywords -> str | [str]: keywords to search for images
+max_images -> int | [int]: max number of images to return
 **properties -> dict: holds any other property of stock's API in a dictionary
 
 Returns
@@ -41,73 +41,113 @@ list:
     a list of images url
 """
 
-def get_images(stock, keywords, max_images, **properties):
+def main_trigger(stock, keywords, max_images, **properties):
     ret = {}
-    try:
-        if stock == "pixable":
-            pixable = stocks.Pixable()
-            if type(max_images) is int and type(keywords) is str:
-                ret[keywords] = pixable.get_pictures(keywords, max_images, **properties)
+    if stock == "pixable":
+        pixable = stocks.Pixable()
+        if type(max_images) is int and type(keywords) is str:
+            ans = pixable.get_pictures(keywords, max_images, **properties)
+            if type(ans) is int:
+                return "error: " + str(ans)
+            ret[keywords] = ans
 
-            elif type(max_images) is int and type(keywords) is list:
-                for keyword in keywords:
-                    ret[keyword] = pixable.get_pictures(keyword, max_images, **properties)
+        elif type(max_images) is int and type(keywords) is list:
+            for keyword in keywords:
+                ans = pixable.get_pictures(keyword, max_images, **properties)
+                if type(ans) is int:
+                    return "error: " + str(ans)
+                ret[keyword] = ans
 
-            elif type(max_images) is list and type(keywords) is list:
-                for keyword in keywords:
-                    ret[keyword] = pixable.get_pictures(keyword, max_images[keywords.index(keyword)], **properties)
+        elif type(max_images) is list and type(keywords) is list:
+            for keyword in keywords:
+                ans = pixable.get_pictures(keyword, max_images[keywords.index(keyword)], **properties)
+                if type(ans) is int:
+                    return "error: " + str(ans)
+                ret[keyword] = ans
 
-        elif stock == "shutterstock":
-            shutterstock = stocks.Shutterstock()
-            if type(max_images) is int and type(keywords) is str:
-                ret[keywords] = shutterstock.get_pictures(keywords, max_images, **properties)
+    elif stock == "shutterstock":
+        shutterstock = stocks.Shutterstock()
+        if type(max_images) is int and type(keywords) is str:
+            ans = shutterstock.get_pictures(keywords, max_images, **properties)
+            if type(ans) is int:
+                return "error: " + str(ans)
+            ret[keywords] = ans
 
-            elif type(max_images) is int and type(keywords) is list:
-                for keyword in keywords:
-                    ret[keyword] = shutterstock.get_pictures(keyword, max_images, **properties)
+        elif type(max_images) is int and type(keywords) is list:
+            for keyword in keywords:
+                ans = shutterstock.get_pictures(keyword, max_images, **properties)
+                if type(ans) is int:
+                    return "error: " + str(ans)
+                ret[keyword] = ans
 
-            elif type(max_images) is list and type(keywords) is list:
-                for keyword in keywords:
-                    ret[keyword] = shutterstock.get_pictures(keyword, max_images[keywords.index(keyword)], **properties)
+        elif type(max_images) is list and type(keywords) is list:
+            for keyword in keywords:
+                ans = shutterstock.get_pictures(keyword, max_images[keywords.index(keyword)], **properties)
+                if type(ans) is int:
+                    return "error: " + str(ans)
+                ret[keyword] = ans
 
-    except:
-        ret = {"error": "error occurred"}
 
     json_obj = json.dumps(ret)
     return json_obj
 
+
 def main():
-   out =get_images("pixable", ["banana", "apple"], 5, colors = "green")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out =main_trigger("pixable", ["banana", "apple"], 5, colors ="green")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("shutterstock", ["dog", "cat"], 5, colors="green")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("shutterstock", ["dog", "cat"], 5, colors="green")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("pixable", "sun shine", 5, colors="yellow")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("pixable", "sun shine", 5, colors="yellow")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("shutterstock", "ground", 5, colors="yellow")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("shutterstock", "ground", 5, colors="yellow")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("pixable", ["banana", "apple"], [1, 2], colors="green")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("pixable", ["banana", "apple"], [1, 2], colors="green")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("shutterstock", ["dog", "cat"], [3, 4], colors="green")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("shutterstock", ["dog", "cat"], [3, 4], colors="green")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("pixable", "sun shine", 5, colors="yellow")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("pixable", "sun shine", 5, colors="yellow")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
-   out = get_images("shutterstock", "ground", 5, colors="yellow")
-   parsed = json.loads(out)
-   print(json.dumps(parsed, indent=4, sort_keys=True))
+   out = main_trigger("shutterstock", "ground", 5, colors="yellow")
+   try:
+       parsed = json.loads(out)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+   except:
+       print(out)
 
 
 
