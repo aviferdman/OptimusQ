b74cc92290723de26fc1913a681ea1d93a403182
Gil <gilhat@post.bgu.ac.il>
2021-12-23 13:45:50 +0200

    images update

--- ImageService/main.py
+++ ImageService/main.py
@@ -1,6 +1,9 @@
 import azure.functions
 import string
-import stocks
+try:
+    from . import stocks
+except:
+    import stocks
 import json
 
 def read_data():
@@ -49,335 +52,233 @@ def main_trigger(req: azure.functions.HttpRequest):
     try:
         js = req.get_json()
     except:
-        return "Empty Body"
+        return {"images": {}}
     stock = js["stock"]
     stock = stock.translate({ord(i): None for i in string.whitespace})
     keywords = js["keywords"]
-    max_images = js["max_images"]
+    max_images = js["maxImages"]
     properties = js["properties"]
     valid = validInput(stock, keywords, max_images, properties)
-    if not valid[0]:
-        return "Error: " + valid[1]
+    if not valid:
+        return {"images": {}}
 
     if stock == "pixable":
         ret = getImagePixable(keywords, max_images, properties)
     elif stock == "shutterstock":
         ret = getImageShutterStock(keywords, max_images, properties)
 
-    if type(ret) is not dict:
-        return ret
 
-    json_obj = json.dumps(ret)
-    return json_obj
+    return {"images": ret}
 
 def main_trigger_json(js):
     ret = None
-    # js = req.get_json()
-    js = json.loads(js)
-    if not js:
-        return "Empty Json"
     stock = js["stock"]
     stock = stock.translate({ord(i): None for i in string.whitespace})
     keywords = js["keywords"]
-    max_images = js["max_images"]
+    max_images = js["maxImages"]
     properties = js["properties"]
     valid = validInput(stock, keywords, max_images, properties)
-    if not valid[0]:
-        return "Error: " + valid[1]
+    if not valid:
+        return {"images": {}}
 
     if stock == "pixable":
         ret = getImagePixable(keywords, max_images, properties)
     elif stock == "shutterstock":
         ret = getImageShutterStock(keywords, max_images, properties)
 
-    if type(ret) is not dict:
-        return ret
-
-    json_obj = json.dumps(ret)
-    return json_obj
+    return {"images": ret}
 
 def validInput(stock, keywords, max_images, properties):
     if stock != "pixable" and stock != "shutterstock":
-        return False, "stock name should be from: {pixable, shutterstock}"
+        return False
 
     if type(keywords) is not list or type(max_images) is not list or type(properties) is not dict:
-        return False, "type error"
+        return False
 
     if len(keywords) != len(max_images):
-        return False, "list length error"
+        return False
 
     if not(all(isinstance(item, str) for item in keywords) and all(isinstance(item, int) for item in max_images)):
-        return False, "list type error"
+        return False
 
-    return True,
-
-# def main_trigger(stock, keywords, max_images, **properties):
-#     error = False
-#     ret = {}
-#     if stock == "pixable":
-#         if type(max_images) is int and type(keywords) is str:
-#             error = getImagePixable(keywords, max_images, ret, **properties)
-#
-#         elif type(max_images) is int and type(keywords) is list:
-#             for keyword in keywords:
-#                 error = getImagePixable(keyword, max_images, ret, **properties)
-#
-#         elif type(max_images) is list and type(keywords) is list:
-#             for keyword in keywords:
-#                 error = getImagePixable(keyword, max_images[keywords.index(keyword)], ret, **properties)
-#
-#     elif stock == "shutterstock":
-#         if type(max_images) is int and type(keywords) is str:
-#             error = getImageShutterStock(keywords, max_images, ret, **properties)
-#
-#         elif type(max_images) is int and type(keywords) is list:
-#             for keyword in keywords:
-#                 error = getImageShutterStock(keyword, max_images, ret, **properties)
-#
-#         elif type(max_images) is list and type(keywords) is list:
-#             for keyword in keywords:
-#                 error = getImageShutterStock(keyword, max_images[keywords.index(keyword)], ret, **properties)
-#
-#     if error:
-#         return "Error: " + str(error)
-#
-#     json_obj = json.dumps(ret)
-#     return json_obj
-
-# def getImagePixable(keywords, max_images, ret, **properties):
-#     try:
-#         pixable = stocks.Pixable()
-#         ans = pixable.get_pictures(keywords, max_images, **properties)
-#         if type(ans) is int:
-#             raise Exception("Error: " + str(ans))
-#
-#     except:
-#         error = ans
-#         return error
-#
-#     ret[keywords] = ans
-#
-# def getImageShutterStock(keywords, max_images, ret, **properties):
-#     try:
-#         shutterstock = stocks.Shutterstock()
-#         ans = shutterstock.get_pictures(keywords, max_images, **properties)
-#         if type(ans) is int:
-#             raise Exception("Error: " + str(ans))
-#     except:
-#         error = ans
-#         return error
-#
-#     ret[keywords] = ans
+    return True
 
 def getImagePixable(keywords, max_images, properties):
     ret = {}
-    try:
-        pixable = stocks.Pixable()
-        for keyword in keywords:
+    pixable = stocks.Pixable()
+    for keyword in keywords:
+        try:
             ans = pixable.get_pictures(keyword, max_images[keywords.index(keyword)], properties)
             if type(ans) is int:
                 raise Exception("Error: " + str(ans))
             ret[keyword] = ans
 
-    except:
-        error = ans
-        return "error: " + str(error)
+        except:
+            # need to record error in logger
+            continue
 
     return ret
 
 def getImageShutterStock(keywords, max_images, properties):
     ret = {}
-    try:
-        shutterstock = stocks.Shutterstock()
-        for keyword in keywords:
-            ans = shutterstock.get_pictures(keyword, max_images[keywords.index(keyword)], properties)
+    shutterStock = stocks.Shutterstock()
+    for keyword in keywords:
+        try:
+            ans = shutterStock.get_pictures(keyword, max_images[keywords.index(keyword)], properties)
             if type(ans) is int:
                 raise Exception("Error: " + str(ans))
             ret[keyword] = ans
 
-    except:
-        error = ans
-        return "error: " + str(error)
+        except:
+            # need to record error in logger
+            continue
 
     return ret
 
 def test():
     dict_1 = {
         "stock": "shutterstock",
-        "keywords": ["banana", "apple"],
-        "max_images": [3, 1],
+        "keywords": ["green banana", "apple"],
+        "maxImages": [3, 1],
         "properties": {}
     }
 
     dict_2 = {
         "stock": "shutterstock",
         "keywords": ["apple"],
-        "max_images": [3, 1],
+        "maxImages": [3, 1],
         "properties": {}
     }
 
     dict_3 = {
         "stock": "shutterstock",
         "keywords": ["banana", "apple"],
-        "max_images": [3],
+        "maxImages": [3],
         "properties": {}
     }
 
     dict_4 = {
         "stock": "shutterstock",
         "keywords": ["banana", "apple"],
-        "max_images": [3, 1],
+        "maxImages": [3, 1],
         "properties": "wowow"
     }
 
     dict_5 = {
         "stock": "pixable",
         "keywords": [],
-        "max_images": [],
+        "maxImages": [],
         "properties": {}
     }
 
     dict_6 = {
         "stock": "pixable",
         "keywords": ["sky"],
-        "max_images": [7],
+        "maxImages": [7],
         "properties": {}
     }
 
     dict_7 = {
         "stock": "pixable",
         "keywords": ["banana", "apple"],
-        "max_images": [3],
+        "maxImages": [3],
         "properties": {}
     }
 
     dict_8 = {
         "stock": "pixable",
         "keywords": ["apple"],
-        "max_images": [3, 1],
+        "maxImages": [3, 1],
         "properties": "nana"
     }
 
     dict_9 = {
         "stock": "pixable",
         "keywords": ["apple"],
-        "max_images": [3, "banana"],
+        "maxImages": [3, "banana"],
         "properties": {}
     }
 
     dict_10 = {
         "stock": "pixable",
         "keywords": ["apple", 3],
-        "max_images": [3, 1],
+        "maxImages": [3, 1],
         "properties": {}
     }
 
     dict_11 = {
         "stock": "shoko",
         "keywords": ["sky"],
-        "max_images": [7],
+        "maxImages": [7],
         "properties": {}
     }
 
-    js_1 = json.dumps(dict_1, indent=4)
-    js_2 = json.dumps(dict_2, indent=4)
-    js_3 = json.dumps(dict_3, indent=4)
-    js_4 = json.dumps(dict_4, indent=4)
-    js_5 = json.dumps(dict_5, indent=4)
-    js_6 = json.dumps(dict_6, indent=4)
-    js_7 = json.dumps(dict_7, indent=4)
-    js_8 = json.dumps(dict_8, indent=4)
-    js_9 = json.dumps(dict_9, indent=4)
-    js_10 = json.dumps(dict_10, indent=4)
-    js_11 = json.dumps(dict_11, indent=4)
 
     print("1:")
-    out = main_trigger_json(js_1)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_1)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("2:")
-    out = main_trigger_json(js_2)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_2)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("3:")
-    out = main_trigger_json(js_3)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_3)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("4:")
-    out = main_trigger_json(js_4)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_4)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("5:")
-    out = main_trigger_json(js_5)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_5)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("6:")
-    out = main_trigger_json(js_6)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_6)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("7:")
-    out = main_trigger_json(js_7)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_7)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("8:")
-    out = main_trigger_json(js_8)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_8)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("9:")
-    out = main_trigger_json(js_9)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_9)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("10:")
-    out = main_trigger_json(js_10)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_10)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
     print("11:")
-    out = main_trigger_json(js_11)
-    try:
-        parsed = json.loads(out)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
-    except:
-        print(out)
+    out = main_trigger_json(dict_11)
+    images = out["images"]
+    images = json.dumps(images, indent=4)
+    print(images)
 
 def main():
     test()
