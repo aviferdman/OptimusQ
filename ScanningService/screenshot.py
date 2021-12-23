4581feb95c270a913862b74961a02601b71da201
rulidor <rulidor90@gmail.com>
2021-12-23 16:34:50 +0200

    added a whole-page-screenshot scraper

--- ScanningService/screenshot.py
+++ ScanningService/screenshot.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-from time import sleep
-
-# driver = webdriver.Firefox()
-# driver = webdriver.Chrome()
-from webdriver_manager.chrome import ChromeDriverManager
-
+#
+# from time import sleep
+#
+# # driver = webdriver.Firefox()
+# # driver = webdriver.Chrome()
+# from webdriver_manager.chrome import ChromeDriverManager
+#
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 #
 # driver.get('https://www.bbc.com/storyworks/clear-sky-thinking-airbus-2021/airbus-2021-clear-sky-thinking-?utm_source=taboola&utm_medium=native&tblci=GiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyCLjFQojKLYzprtvuKHAQ#tblciGiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyCLjFQojKLYzprtvuKHAQ')
@@ -14,15 +14,57 @@ from webdriver_manager.chrome import ChromeDriverManager
 #
 # driver.get_screenshot_as_file("screenshot.png")
 # driver.quit()
+#**********************************
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.maximize_window()
+# driver.get('https://www.bbc.com/storyworks/clear-sky-thinking-airbus-2021/airbus-2021-clear-sky-thinking-?utm_source=taboola&utm_medium=native&tblci=GiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyCLjFQojKLYzprtvuKHAQ#tblciGiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyCLjFQojKLYzprtvuKHAQ')
+# scheight = .1
+# while scheight < 9.9:
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight/%s);" % scheight)
+#     scheight += .01
+# driver.save_screenshot('screenshot.png')
+# ***********************************
+from selenium import webdriver
+from PIL import Image
+from io import StringIO, BytesIO
+# from cStringIO import StringIO
+from webdriver_manager.chrome import ChromeDriverManager
 
-url = 'https://stackoverflow.com/'
-path = 'scrapedScreen.png'
+verbose = 1
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(url)
-# el = driver.find_element_by_tag_name('body')
-el = driver.find_element(By.CLASS_NAME, "body")
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get('https://www.ynet.co.il')
 
-el.screenshot(path)
-driver.quit()
-print("end...")
+# from here http://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
+js = 'return Math.max( document.body.scrollHeight, document.body.offsetHeight,  document.documentElement.clientHeight,  document.documentElement.scrollHeight,  document.documentElement.offsetHeight);'
+
+scrollheight = browser.execute_script(js)
+
+if verbose > 0:
+    print (scrollheight)
+
+slices = []
+offset = 0
+while offset < scrollheight:
+    if verbose > 0:
+        print (offset)
+
+    browser.execute_script("window.scrollTo(0, %s);" % offset)
+    img = Image.open(BytesIO(browser.get_screenshot_as_png()))
+    offset += img.size[1]
+    slices.append(img)
+
+    if verbose > 0:
+        browser.get_screenshot_as_file('%s/screen_%s.png' % ('/tmp', offset))
+        print (scrollheight)
+
+
+screenshot = Image.new('RGB', (slices[0].size[0], scrollheight))
+offset = 0
+for img in slices:
+    screenshot.paste(img, (0, offset))
+    offset += img.size[1]
+
+screenshot.save('test.png')
