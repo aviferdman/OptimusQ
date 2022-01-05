4874f02cdbeac10b88508781e6a690f733caafe9
rulidor <rulidor90@gmail.com>
2022-01-05 11:59:12 +0200

    added comments to scanner tests

--- Tests/UnitTests/ScannerServiceUnitTests/test_recoSystem.py
+++ Tests/UnitTests/ScannerServiceUnitTests/test_recoSystem.py
@@ -6,10 +6,13 @@ from ScanningService import recoSystem
 
 class TestRecoSystem(unittest.TestCase):
 
+    # create RecoSystem
     def setUp(self):
         self.reco = recoSystem.RecoSystem()
 
 
+    # reads url from the valid file, for each of the urls.
+    # asserts the return is not "cant open the url"
     def test_valid_url(self):
         i = 0
         data = pd.read_excel(r'valid.xlsx')
@@ -24,12 +27,14 @@ class TestRecoSystem(unittest.TestCase):
                 self.assertNotEqual(title, "cant open the url")
                 self.assertNotEqual(description, "cant open the url")
                 self.assertNotEqual(keywords, "cant open the url")
-                self.assertNotEqual(keywords, "Cannot extract keywords")
+                self.assertNotEqual(keywords, [])
 
             except Exception as e:
                 print("index: " + str(i) + ": " + str(e))
             i = i + 1
 
+    # reads url from the NON-valid file, for each of the urls.
+    # asserts the return is "cant open the url", for each one.
     def test_problem_url(self):
         i = 0
         data = pd.read_excel(r'problem.xlsx')
@@ -43,7 +48,8 @@ class TestRecoSystem(unittest.TestCase):
                 keywords = dict['keywords']
                 self.assertEqual(title, "cant open the url")
                 self.assertEqual(description, "cant open the url")
-                self.assertEqual(keywords, "[]")
+                self.assertEqual(keywords, [])
+                print('ok')
 
             except Exception as e:
                 print("index: " + str(i) + ": " + str(e))
