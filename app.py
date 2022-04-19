a16ed6fedfd7fab6561a6856207fdd9e6e454a22
rulidor <rulidor90@gmail.com>
2022-04-19 22:41:58 +0300

    added fb login

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def extract_kw():
 def fb_logged_in():
     # addrss = request.args.get('url')
     if request is not None:
-        addrss = request.get_json()
-
+        req_json = request.get_json()
+        print(req_json)
     return render_template("fb_logged_in.html")
 
 
