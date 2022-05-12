d10ce157fd11c22762cc0c9a37cae7f34e2a814b
rulidor <rulidor90@gmail.com>
2022-05-12 21:54:06 +0300

    fixed requirements

--- GoogleAdsService/CampaignManagement.py
+++ GoogleAdsService/CampaignManagement.py
@@ -24,7 +24,7 @@ client_secret = token_details['client_secret']
 refresh_token = token_details['refresh_token']
 
 token_dict = {"developer_token": developer_token, "refresh_token": refresh_token, "client_id": client_id,
-              "client_secret": client_secret}
+              "client_secret": client_secret, "use_proto_plus": True}
 
 
 # This interface allows the user to create and manage all the marketing fields,
